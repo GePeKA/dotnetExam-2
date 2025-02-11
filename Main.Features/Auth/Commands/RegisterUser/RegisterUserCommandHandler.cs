77e1f87154b4ebbdf42bb60a5146a25f1116cb13
Maxim Lozhkin <maximlozhking2004@gmail.com>
2025-02-11 18:15:43 +0300

    Добавил отправление сообщения при создании пользователя

--- Main.Features/Auth/Commands/RegisterUser/RegisterUserCommandHandler.cs
+++ Main.Features/Auth/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -4,41 +4,53 @@ using Main.Infrastructure.Services.Abstractions;
 using Main.Infrastructure.UnitOfWork;
 using Shared.CQRS.Commands;
 using Shared.DTO;
+using Shared.MessagingContracts;
 
 namespace Main.Features.Auth.Commands.RegisterUser
 {
     public class RegisterUserCommandHandler(
+        IRatingCommunicationService ratingCommunicationService,
         IUserRepository userRepository,
         IUnitOfWork unitOfWork,
         IHasherService hasherService) : ICommandHandler<RegisterUserCommand, RegisterUserDto>
     {
         public async Task<Result<RegisterUserDto>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
         {
-            if (string.IsNullOrWhiteSpace(request.UserName) 
+            try
+            {
+                if (string.IsNullOrWhiteSpace(request.UserName)
                 || string.IsNullOrWhiteSpace(request.Password))
-            {
-                return new Result<RegisterUserDto>(null, false, "Username and password must not be empty!");
+                {
+                    return new Result<RegisterUserDto>(null, false, "Username and password must not be empty!");
+                }
+
+                var isUserWithUsernameExist = (await userRepository.GetUserAsync(request.UserName)) != null;
+
+                if (isUserWithUsernameExist)
+                {
+                    return new Result<RegisterUserDto>(null, false, "User with such username already exists!");
+                }
+
+                var hashedPassword = hasherService.Hash(request.Password);
+
+                var user = new User()
+                {
+                    UserName = request.UserName,
+                    Password = hashedPassword
+                };
+
+                var createdUsername = await userRepository.AddUserAsync(user);
+                await unitOfWork.SaveChangesAsync();
+
+                var userCreatedEvent = new UserCreatedEvent(createdUsername);
+                await ratingCommunicationService.SendUserCreatedEvent(userCreatedEvent);
+
+                return new Result<RegisterUserDto>(new RegisterUserDto(createdUsername), true);
             }
-
-            var isUserWithUsernameExist = (await userRepository.GetUserAsync(request.UserName)) != null;
-
-            if (isUserWithUsernameExist)
+            catch (Exception ex)
             {
-                return new Result<RegisterUserDto>(null, false, "User with such username already exists!");
+                return new Result<RegisterUserDto>(null, false, ex.Message);
             }
-
-            var hashedPassword = hasherService.Hash(request.Password);
-
-            var user = new User()
-            {
-                UserName = request.UserName,
-                Password = hashedPassword
-            };
-
-            var createdUsername = await userRepository.AddUserAsync(user);
-            await unitOfWork.SaveChangesAsync();
-
-            return new Result<RegisterUserDto>(new RegisterUserDto(createdUsername), true);
         }
     }
 }
