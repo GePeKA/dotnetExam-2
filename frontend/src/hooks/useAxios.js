40c87ed02b4a396525f29aac1912951553177daa
Maxim Lozhkin <maximlozhking2004@gmail.com>
2025-02-11 23:53:27 +0300

    Front 2

--- frontend/src/hooks/useAxios.js
+++ frontend/src/hooks/useAxios.js
@@ -12,12 +12,6 @@ export const axiosToRatingService = axios.create({
     timeout: 30000
 });
 
-export const axiosToRatingService = axios.create({
-    baseURL: `http://localhost:9080/rating-api`,
-    withCredentials: true,
-    timeout: 30000
-});
-
 axiosToBackend.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
     return config;
