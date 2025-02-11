c0579b19d0013bed2c00f60dcb06895d7924992c
BikbaevMV <MVBikbaev@stud.kpfu.ru>
2025-02-11 13:14:16 +0300

    frontend add game page

--- frontend/src/assets/icons.jsx
+++ frontend/src/assets/icons.jsx
@@ -1,6 +1,6 @@
 const ICONS = {
-    circle: new URL('./icons/circle.png', import.meta.url).href,
-    cross: new URL('./icons/cross.png', import.meta.url).href,
+    circle: new URL('./icons/circle.svg', import.meta.url).href,
+    cross: new URL('./icons/cross.svg', import.meta.url).href,
     game: new URL('./icons/game.png', import.meta.url).href,
     settings: new URL('./icons/settings.svg', import.meta.url).href,
     sign_in: new URL('./icons/sign_in.svg', import.meta.url).href,
